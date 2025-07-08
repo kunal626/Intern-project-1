ffbf4a38991f7d7cd80cd34af578803f17568fdc
kunal626 <147721328+kunal626@users.noreply.github.com>
2025-07-09 00:26:57 +0530

    updated project

--- frontend/src/components/Layout/Footer.jsx
+++ frontend/src/components/Layout/Footer.jsx
@@ -2,18 +2,16 @@ import React, { useContext } from 'react'
 import {Context} from "../../main"
 import {Link} from "react-router-dom"
 import { FaGithub , FaLinkedin} from "react-icons/fa"
-import { SiLeetcode } from "react-icons/si";
 import { RiInstagramFill} from "react-icons/ri"
 function Footer() {
   const {isAuthorized}  = useContext(Context)
   return (
     <footer className= {isAuthorized ? "footerShow" : "footerHide"}>
-<div>&copy; All Rights Reserved by Abhishek.</div>
+<div>&copy; Job Portal Website - By Kunal.</div>
 <div>
-  <Link to={'https://github.com/exclusiveabhi'} target='github'><FaGithub></FaGithub></Link>
-  <Link to={'https://leetcode.com/u/exclusiveabhi/'} target='leetcode'><SiLeetcode></SiLeetcode></Link>
-  <Link to={'https://www.linkedin.com/in/abhishek-rajput-/'} target='linkedin'><FaLinkedin></FaLinkedin></Link>
-  <Link to={'https://www.instagram.com/exclusiveabhi/'} target='instagram'><RiInstagramFill></RiInstagramFill></Link>
+  <Link to={'https://github.com/kunal626'} target='github'><FaGithub></FaGithub></Link>
+  <Link to={'https://www.linkedin.com/in/kunal-bansode-170a7a36a/'} target='linkedin'><FaLinkedin></FaLinkedin></Link>
+  <Link to={'https://www.instagram.com/i_am_kunal_666/'} target='instagram'><RiInstagramFill></RiInstagramFill></Link>
 </div>
       
     </footer>
